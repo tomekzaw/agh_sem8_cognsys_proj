e333e719e0e31a32443690cff6c0398ecbb17302
jakubdybczak <jakub.dybczak@gmail.com>
2021-05-17 00:36:43 +0200

    Add continous scanning

--- app/App.js
+++ app/App.js
@@ -6,9 +6,10 @@
  * @flow strict-local
  */
 
-import {Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import {BleManager} from 'react-native-ble-plx';
+import { BleManager } from 'react-native-ble-plx';
+import base64 from 'react-native-base64'
 import React from 'react';
 
 const bleManager = new BleManager();
@@ -16,17 +17,21 @@ const serviceUUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
 const characteristicUUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 
 const App = () => {
-  const [text, setText] = React.useState('');
+  const [text, setText] = React.useState('Disconnected');
   const [disabled, setDisabled] = React.useState(false);
+  const [devices, setDevices] = React.useState({});
 
-  const handlePress = async () => {
+  const handleConnectPress = async () => {
     console.log('Scan in progress...');
     setText('Scan in progress...');
     setDisabled(true);
 
+    // start device scan when app is resumed
     await bleManager.startDeviceScan(
       [serviceUUID],
-      null,
+      {
+        "allowDuplicates": true
+      },
       async (error, device) => {
         if (error) {
           // TODO: handle Bluetooth adapter or Location off
@@ -36,30 +41,59 @@ const App = () => {
           return;
         }
 
-        await bleManager.stopDeviceScan();
+        if (devices[device.id]) {
+          return;
+        }
 
-        await bleManager.connectToDevice(device.id);
-        console.log(device.name);
+        console.log("Scanned device: " + device.name);
+        devices[device.id] = device;
+        setDevices(devices);
+        // setDevices is only to ensure that no other scan will connect to device in the same time
+
+        try {
+          await bleManager.connectToDevice(device.id);
+        } catch (e) {
+          devices[device.id] = null;
+          setDevices(devices);
+        }
 
         await device.discoverAllServicesAndCharacteristics();
         const characteristic = await device.readCharacteristicForService(
           serviceUUID,
           characteristicUUID,
         );
-        console.log(characteristic.value);
 
-        await device.cancelConnection();
+        setText(base64.decode(characteristic.value))
 
-        console.log('Scan finished');
-        setText('Scan finished');
+        subscription = characteristic.monitor(async (error, characteristic) => {
+          if (!error) {
+            setText(base64.decode(characteristic.value))
+          } else {
+            setText("Disconnected");
+            console.log("Device disconnected");
+            try {
+              await devices[device.id].cancelConnection();
+            } catch (e) {
+            }
+            devices[device.id] = null;
+            setDevices(devices);
+          }
+        })
+
+        console.log("Device connected");
         setDisabled(false);
       },
     );
   };
 
+  const handleDisconnectPress = async () => {
+    await bleManager.stopDeviceScan();
+  }
+
   return (
     <View style={styles.containerStyle}>
-      <Button title="Click me!" onPress={handlePress} disabled={disabled} />
+      <Button title="Click me to start scanning!" onPress={handleConnectPress} disabled={disabled} />
+      <Button title="Click me to stop scanning!" onPress={handleDisconnectPress} disabled={disabled} />
       <Text>{text}</Text>
     </View>
   );
@@ -70,7 +104,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    backgroundColor: "#FFF"
+  }
 });
 
 export default App;
--- esp/BLE_server/BLE_server.ino
+++ esp/BLE_server/BLE_server.ino
@@ -1,45 +1,83 @@
-/*
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updates by chegewara
-*/
-
 #include <BLEDevice.h>
-#include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      pServer->startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
 
-  BLEDevice::init("Long name works now");
-  BLEServer *pServer = BLEDevice::createServer();
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
 
-  pCharacteristic->setValue("Hello World says Neil");
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+
+  // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000);
+    // notify changed value
+    if (deviceConnected) {
+        pCharacteristic->setValue(value % 2 == 0 ? "RED" : "GREEN");
+        pCharacteristic->notify();
+        value++;
+        delay(5000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+        Serial.println("Still notifying...");
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
