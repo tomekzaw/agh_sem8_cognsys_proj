5db40fe769f1cd6fa503d99afc3481b824742c99
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2021-05-17 12:34:16 +0200

    Setup react-native-tts

--- app/App.js
+++ app/App.js
@@ -6,11 +6,12 @@
  * @flow strict-local
  */
 
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-import { BleManager } from 'react-native-ble-plx';
-import base64 from 'react-native-base64'
+import {BleManager} from 'react-native-ble-plx';
 import React from 'react';
+import Tts from 'react-native-tts';
+import base64 from 'react-native-base64';
 
 const bleManager = new BleManager();
 const serviceUUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
@@ -30,7 +31,7 @@ const App = () => {
     await bleManager.startDeviceScan(
       [serviceUUID],
       {
-        "allowDuplicates": true
+        allowDuplicates: true,
       },
       async (error, device) => {
         if (error) {
@@ -45,7 +46,7 @@ const App = () => {
           return;
         }
 
-        console.log("Scanned device: " + device.name);
+        console.log('Scanned device: ' + device.name);
         devices[device.id] = device;
         setDevices(devices);
         // setDevices is only to ensure that no other scan will connect to device in the same time
@@ -63,24 +64,25 @@ const App = () => {
           characteristicUUID,
         );
 
-        setText(base64.decode(characteristic.value))
+        setText(base64.decode(characteristic.value));
 
-        subscription = characteristic.monitor(async (error, characteristic) => {
-          if (!error) {
-            setText(base64.decode(characteristic.value))
-          } else {
-            setText("Disconnected");
-            console.log("Device disconnected");
-            try {
-              await devices[device.id].cancelConnection();
-            } catch (e) {
+        const subscription = characteristic.monitor(
+          async (error, characteristic) => {
+            if (!error) {
+              setText(base64.decode(characteristic.value));
+            } else {
+              setText('Disconnected');
+              console.log('Device disconnected');
+              try {
+                await devices[device.id].cancelConnection();
+              } catch (e) {}
+              devices[device.id] = null;
+              setDevices(devices);
             }
-            devices[device.id] = null;
-            setDevices(devices);
-          }
-        })
+          },
+        );
 
-        console.log("Device connected");
+        console.log('Device connected');
         setDisabled(false);
       },
     );
@@ -88,12 +90,31 @@ const App = () => {
 
   const handleDisconnectPress = async () => {
     await bleManager.stopDeviceScan();
-  }
+  };
+
+  const handleSaySomething = async () => {
+    Tts.getInitStatus().then(() => {
+      // Tts.setDefaultLanguage('en-GB');
+      Tts.speak('Linia 139. Kierunek: Miasteczko Studenckie AGH');
+      Tts.speak('Drzwi otwarte. Proszę wsiadać.');
+      Tts.speak('Światło czerwone. Pozostało 25 sekund.');
+      Tts.speak('Światło zielone.');
+    });
+  };
 
   return (
     <View style={styles.containerStyle}>
-      <Button title="Click me to start scanning!" onPress={handleConnectPress} disabled={disabled} />
-      <Button title="Click me to stop scanning!" onPress={handleDisconnectPress} disabled={disabled} />
+      <Button
+        title="Click me to start scanning!"
+        onPress={handleConnectPress}
+        disabled={disabled}
+      />
+      <Button
+        title="Click me to stop scanning!"
+        onPress={handleDisconnectPress}
+        disabled={disabled}
+      />
+      <Button title="Say something!" onPress={handleSaySomething} />
       <Text>{text}</Text>
     </View>
   );
@@ -104,8 +125,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "#FFF"
-  }
+    backgroundColor: '#fff',
+  },
 });
 
 export default App;
